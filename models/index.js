8007fdbe7bbb841e60a9d019a47bf08e817b5dd6
josephdaw <josephdaw@icloud.com>
2021-12-02 20:56:09 +1100

    fixing duplicated product and tag ID returns

--- models/index.js
+++ models/index.js
@@ -18,25 +18,15 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   // Define the third table needed to store the foreign keys
-  through: {
-    model: ProductTag,
-    unique: true,
-    foreignKey: 'product_id'
-  },
-  // Define an alias for when data is retrieved
-  // as: 'tags'
+  through: ProductTag, 
+  foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   // Define the third table needed to store the foreign keys
-  through: {
-    model: ProductTag,
-    unique: true,
+  through: ProductTag,
     foreignKey: 'tag_id',
-  },
-  // Define an alias for when data is retrieved
-  // as: 'tagged_products'
 });
 
 module.exports = {
