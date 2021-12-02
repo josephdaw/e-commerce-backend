7265af3a7127875b3182d921eb2409793821b926
josephdaw <josephdaw@icloud.com>
2021-12-02 23:58:55 +1100

    adding post route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,9 +16,8 @@ router.get('/', async (req, res) => {
   }
 });
 
+// find a single Tag by its `id` including its associated Product data
 router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
   try {
     const tagsData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product}]
@@ -31,8 +30,16 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
 });
 
 router.put('/:id', (req, res) => {
