d2c1f99cb0b3b7e0f914c2ada486d573506d204b
josephdaw <josephdaw@icloud.com>
2021-12-03 00:05:56 +1100

    adding put route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -42,8 +42,21 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+// update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+
+    res.status(200).json(tagData);
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
 });
 
 // delete on tag by its `id` value
